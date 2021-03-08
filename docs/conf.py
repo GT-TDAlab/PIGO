9fd3bff5c9eadaed018698bb7256dc25879d57c6
Kasimir Gabert <kasimir@gatech.edu>
2021-03-08 11:54:43 -0500

    Updating version

--- docs/conf.py
+++ docs/conf.py
@@ -27,7 +27,7 @@ author = 'GT - TDA Lab'
 # The short X.Y version
 version = ''
 # The full version, including alpha/beta/rc tags
-release = '1.0.0'
+release = '0.5.0'
 
 # -- General configuration ---------------------------------------------------
 
